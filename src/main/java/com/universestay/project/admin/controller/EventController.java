ad5fc6f1529726547a037b709bd9c2f010234abb
hozn_e <hwlsdl37@gmail.com>
2023-11-24 15:01:25 +0900

    feat: 채팅 메세지, 채팅방 Dto 추가 / 관리자 이벤트 컨트롤러 변경 / 관리자 CSS 디렉토리 변경 (#23)
    * 관리자 이벤트 페이지 Controller 주석처리 !!에러발생..
    관리자 컨트롤러 경로 회의 내용대로 변경
    
    * 이벤트 컨트롤러 주석 해제
    ChattingMessageDto 생성
    ChattingRoomDto 생성
    
    * 이벤트 컨트롤러 주석 해제
    ChattingMessageDto 생성
    ChattingRoomDto 생성
    관리자 css 파일 경로 변경 (/css/common/admin -> /css/admin)
    관리자 이벤트 페이지(event.jsp, eventInput.jsp, eventList.jsp) css 적용 경로 수정

--- src/main/java/com/universestay/project/admin/controller/EventController.java
+++ src/main/java/com/universestay/project/admin/controller/EventController.java
@@ -1,27 +1,28 @@
-//package com.universestay.project.admin.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//
-//@Controller
-//@RequestMapping("/event")
-//public class EventListController {
-//
-//    @GetMapping("/list")
-//    public String list() {
-//        return "eventList";
-//    }
-//
-//    @GetMapping
-//    public String read() {
-//        return "event";
-//    }
-//
-//    @GetMapping("/write")
-//    public String write() {
-//        return "eventInput";
-//    }
-//
-//}
+package com.universestay.project.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/event")
+public class EventController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "admin/eventList";
+    }
+
+    @GetMapping
+    public String read() {
+
+        return "admin/event";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        return "admin/eventInput";
+    }
+
+}
