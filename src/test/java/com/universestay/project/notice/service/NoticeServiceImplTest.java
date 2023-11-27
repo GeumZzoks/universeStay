8d089ae347a8f8c71dff0b8e6626182eab0bc32e
ssunghoon <dtg0209@naver.com>
2023-11-27 22:16:46 +0900

    HOTFIX: 톰캣 실행 안되는 문제로 인한 코드 수정

--- src/test/java/com/universestay/project/notice/service/NoticeServiceImplTest.java
+++ src/test/java/com/universestay/project/notice/service/NoticeServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.universestay.project.notice.service;
 
 import com.universestay.project.notice.dto.NoticeDto;
-import lombok.extern.log4j.Log4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@Log4j
+//@Log4j
 public class NoticeServiceImplTest {
 
     @Autowired
