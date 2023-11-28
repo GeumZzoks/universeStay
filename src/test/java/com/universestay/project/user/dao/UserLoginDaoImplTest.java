8a40143bd3e0a4fb95787e214f0cd6c72e6ddef2
mingming <142335471+m11ng@users.noreply.github.com>
2023-11-28 21:09:41 +0900

    feat: 로그아웃 컨트롤러 추가, 로그인 컨트롤러 맵핑 수정 (#56)
    * feat: 로그아웃 기능 구현
    
    * test: UserLoginDao test 코드 추가

---
+++ src/test/java/com/universestay/project/user/dao/UserLoginDaoImplTest.java
@@ -0,0 +1,26 @@
+package com.universestay.project.user.dao;
+
+import static org.junit.Assert.*;
+
+import com.universestay.project.user.dto.UserDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class UserLoginDaoImplTest {
+
+    @Autowired
+    UserLoginDao userLoginDao;
+
+    @Test
+    public void selectUser() throws Exception {
+        // DB에 유저정보가 있는지 확인
+        // 디비에 진짜 있는 정보 val@gmail.com
+        UserDto userDto = userLoginDao.selectUser("val@gmail.com");
+        assertTrue(userDto != null);
+    }
+}
