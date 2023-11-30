475aa9dcebe2579d000f5cc30b6ebd91436c5f29
beautiflow <1114ny@naver.com>
2023-11-30 15:46:53 +0900

    refactor: 회원가입 API 개발 - JoinServiceImpl 코드 리팩토링

--- src/main/java/com/universestay/project/user/service/JoinServiceImpl.java
+++ src/main/java/com/universestay/project/user/service/JoinServiceImpl.java
@@ -1,5 +1,6 @@
 package com.universestay.project.user.service;
 
+import com.universestay.project.user.dao.ProfileImgDao;
 import com.universestay.project.user.dao.UserJoinDao;
 import com.universestay.project.user.dto.UserDto;
 import java.io.UnsupportedEncodingException;
@@ -20,18 +21,21 @@ public class JoinServiceImpl implements JoinService {
     @Autowired
     UserJoinDao userJoinDao;
 
-    @Override
-    public int registerUser(UserDto userDto) throws Exception {
+    @Autowired
+    ProfileImgDao profileImgDao;
 
-        String uuid = UUID.randomUUID().toString();
+    private String uuid_user_id;
+
+    @Override
+    public int registerUser(UserDto userDto) {
+
+        uuid_user_id = UUID.randomUUID().toString();
         String status_id = "U01";
 
         String user_pwd_encrypt = encrypt(userDto.getUser_email(), userDto.getUser_pwd());
-        System.out.println(user_pwd_encrypt);
 
-        userDto.setUser_id(uuid);
+        userDto.setUser_id(uuid_user_id);
         userDto.setUser_pwd(user_pwd_encrypt);
-        userDto.getUser_pwd();
         userDto.getUser_name();
         userDto.getUser_address();
         userDto.getUser_email();
@@ -39,26 +43,23 @@ public class JoinServiceImpl implements JoinService {
         userDto.getUser_phone_num2();
         userDto.getUser_birth();
         userDto.setStatus_id(status_id);
-        userDto.setCreated_id(uuid);
-        userDto.setUpdated_id(uuid);
-        userDto.getUser_img_url();
+        userDto.setCreated_id(uuid_user_id);
+        userDto.setUpdated_id(uuid_user_id);
 
-        return userJoinDao.insertUser(userDto);
+        try {
+            return userJoinDao.insertUser(userDto);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int checkNickname(String user_nickname) throws Exception {
-        int result = 0;
-
-        System.out.println("user_nickname = " + user_nickname);
-        result = userJoinDao.checkNickname(user_nickname);
-        return result;
+        return userJoinDao.checkNickname(user_nickname);
     }
 
     // 비밀번호 암호화
     private String encrypt(String email, String password) {
-        System.out.println("email = " + email);
-        System.out.println("password = " + password);
         try {
             KeySpec spec = new PBEKeySpec(password.toCharArray(), getSalt(email), 85319, 128);
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
