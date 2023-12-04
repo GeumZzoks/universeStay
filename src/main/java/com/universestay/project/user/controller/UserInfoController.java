9e257b6e4d9a13e9bbbd61e35d17e4c55ad41ecd
Hanna <foureaf@gmail.com>
2023-12-04 18:03:14 +0900

    fix: user info update 에러처리 변경

--- src/main/java/com/universestay/project/user/controller/UserInfoController.java
+++ src/main/java/com/universestay/project/user/controller/UserInfoController.java
@@ -2,15 +2,20 @@ package com.universestay.project.user.controller;
 
 
 import com.universestay.project.user.dto.UserDto;
+import com.universestay.project.user.service.ProfileImgService;
 import com.universestay.project.user.service.UserInfoService;
 import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,6 +27,8 @@ public class UserInfoController {
     @Autowired
     private UserInfoService userInfoService;
 
+    @Autowired
+    private ProfileImgService profileImgService;
 
     //마이페이지 나의 정보 조회
     @GetMapping("/info")
@@ -30,24 +37,38 @@ public class UserInfoController {
         String userEmail = (String) (session.getAttribute("user_email"));
         //user정보를 가져와서 model에 담아 화면으로 반환
         UserDto user = userInfoService.getUserInfo(userEmail);
-        System.out.println(user.toString() + user.getUser_name());
+        String profileImgUrl = profileImgService.getProfileImgUrl(user.getUser_id());
         model.addAttribute("user", user);
+        model.addAttribute("profileImgUrl", profileImgUrl);
         return "/user/myPage/userInfo";
     }
 
     //나의 정보 수정
     @PostMapping("/info/update")
-    public String updateInfo(Model model,
+    public ResponseEntity<String> updateInfo(
             @RequestPart(value = "img", required = false) MultipartFile img,
             @RequestPart(value = "user", required = false) UserDto user,
             HttpSession session) throws Exception {
+        try {
+            userInfoService.updateUserInfo(img != null ? img : null, user);
+            return ResponseEntity.ok("Success");
+        } catch(DuplicateKeyException e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("DuplicateKeyError");
+        }catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ServerError");
+        }
+    }
 
-        System.out.println(img);
-        System.out.println(user);
+    @PostMapping("/info/checkNickname")
+    public ResponseEntity<String> checkNickname(@RequestParam("user_nickname") String user_nickname, HttpSession httpSession)
+            throws Exception {
+        String user_email = (String)httpSession.getAttribute("user_email");
 
-        String userEmail = (String) (session.getAttribute("user_email"));
-        user.setUser_email(userEmail);
-        userInfoService.updateUserInfo(img, user);
-        return "redirect:/user/myPage/info";
+        if( userInfoService.checkNickname(user_email, user_nickname) == "N"){
+            return ResponseEntity.ok("Y");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("N");
     }
 }
