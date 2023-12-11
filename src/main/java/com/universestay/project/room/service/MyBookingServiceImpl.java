1c8d4bf2031c8628be9bf8819373cb411d0d24d5
JKM <simplist119@gmail.com>
2023-12-11 13:04:35 +0900

    Feat: User - 나의 예약내역 구현

---
+++ src/main/java/com/universestay/project/room/service/MyBookingServiceImpl.java
@@ -0,0 +1,26 @@
+package com.universestay.project.room.service;
+
+import com.universestay.project.room.dao.MyBookingDao;
+import com.universestay.project.user.dto.RoomReviewDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class MyBookingServiceImpl implements MyBookingService{
+
+    @Autowired
+    MyBookingDao myBookingDao;
+
+    @Override
+    public List<Map<String, Object>> getMyBookingList(Map map) throws Exception {
+        return myBookingDao.selectMyBookings(map);
+    }
+
+    @Override
+    public int writeRoomReview(RoomReviewDto dto) throws Exception {
+        return myBookingDao.insertRoomReview(dto);
+    }
+}
