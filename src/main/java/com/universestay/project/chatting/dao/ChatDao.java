e796a6a3dd8014c8bafd353d00c42fa681d99f95
beautiflow <1114ny@naver.com>
2023-12-14 18:15:38 +0900

    refactor: 파일 변경 후 삭제

--- src/main/java/com/universestay/project/chatting/dao/ChatDao.java
+++
@@ -1,9 +0,0 @@
-package com.universestay.project.chatting.dao;
-
-import com.universestay.project.dto.ChattingRoomDto;
-
-public interface ChatDao {
-
-    int createRoom(ChattingRoomDto chattingRoomDto) throws Exception;
-
-}
--- src/main/java/com/universestay/project/chatting/service/ChatService.java
+++
@@ -1,8 +0,0 @@
-package com.universestay.project.chatting.service;
-
-import com.universestay.project.dto.ChattingRoomDto;
-
-public interface ChatService {
-
-    int createRoom(ChattingRoomDto chattingRoomDto);
-}
