c41327104977f404904445c8514888fbfbba771d
nick <alskadmlcraz1@gmail.com>
2023-12-18 20:10:36 +0900

    feat: 결제 api 중간 개발 완료 (#187)
    류한나 팀원과 코드 합칠 예정

--- src/main/java/com/universestay/project/payment/controller/PaymentController.java
+++ src/main/java/com/universestay/project/payment/controller/PaymentController.java
@@ -1,10 +1,14 @@
 package com.universestay.project.payment.controller;
 
+import com.universestay.project.payment.service.PaymentService;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,27 +17,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/payment")
+@CrossOrigin(origins = "https://api.iamport.kr")
 public class PaymentController {
+
+    private PaymentService paymentService;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     @GetMapping
     public String payment() {
         return "payment/payment";
     }
 
-    @PostMapping
+    @PostMapping("/getAccessToken")
     @ResponseBody
-    public ResponseEntity payment1(@RequestParam("imp_uid") String imp_uid,
+    public ResponseEntity getAccessToken(@RequestParam("imp_uid") String imp_uid,
             @RequestParam("merchant_uid") String merchant_uid) throws IOException {
 
         URL url = new URL("https://api.iamport.kr/users/getToken");
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
         urlConnection.setRequestMethod("POST");
         urlConnection.setRequestProperty("Accept", "application/json");
+        urlConnection.setDoOutput(true);
 
         return null;
     }
 
-//    @GetMapping
-//    @ResponseBody
-//    public ResponseEntity
+    @PostMapping("/getPaymentInfo")
+    @ResponseBody
+    public ResponseEntity getPaymentInfo(@RequestParam("bookingId") String bookingId) {
+        return new ResponseEntity<>(paymentService.findPaymentUser(bookingId), HttpStatus.OK);
+    }
 }
