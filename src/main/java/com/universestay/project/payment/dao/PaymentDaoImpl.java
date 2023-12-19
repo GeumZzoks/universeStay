f0554c6f7e5d26534195594dd930f25e255cc46c
Ryanna <121004473+hanna-ryu@users.noreply.github.com>
2023-12-19 16:18:29 +0900

    Task 438: 예약 결제 결과 DB 저장하는 API 추가 (#188)
    * feat: Payment Dto, alias 추가
    
    * feat: insertPaymentInfo mapper, dao, service, controller 추가
    
    * feat: db에 결제 정보하는 기능 추가

--- src/main/java/com/universestay/project/payment/dao/PaymentDaoImpl.java
+++ src/main/java/com/universestay/project/payment/dao/PaymentDaoImpl.java
@@ -1,6 +1,7 @@
 package com.universestay.project.payment.dao;
 
 import com.universestay.project.common.exception.CommonException;
+import com.universestay.project.payment.dto.PaymentDto;
 import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,10 @@ public class PaymentDaoImpl implements PaymentDao {
     @Override
     public Map<String, Object> findPaymentUser(String booking_id) throws CommonException {
         return session.selectOne(namespace + "findPaymentUser", booking_id);
+    }
+
+    @Override
+    public int insertPaymentInfo(PaymentDto paymentDto) throws CommonException {
+        return session.insert(namespace + "insertPaymentInfo", paymentDto);
     }
 }
