0d508363b1c7de861a235abdb443b889e08d75af
Hanna <foureaf@gmail.com>
2023-12-22 01:07:09 +0900

    feat: 체크인, 체크아웃, 예약 신청일 형식 변경해서 뿌려주도록 컨트롤러 수정

--- src/main/java/com/universestay/project/user/controller/MyBookingController.java
+++ src/main/java/com/universestay/project/user/controller/MyBookingController.java
@@ -2,6 +2,7 @@ package com.universestay.project.user.controller;
 
 import com.universestay.project.review.dto.RoomReviewDto;
 import com.universestay.project.user.service.MyBookingService;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -43,9 +44,38 @@ public class MyBookingController {
             // 또한, 예약 2건 이상일 수 있으므로 List 형태로 받는다.
             // list2는 이용완료 된 예약건들에 대한 List이며
             // 나머지는 상동하다.
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
             List<Map<String, Object>> list1 = myBookingService.getMyBookingList(map1);
+            System.out.println(list1);
+            System.out.println(
+                    "--------------------------------------------------------------------------------------------------------------------------------");
+            for (int i = 0; i < list1.size(); i++) {
+                String booking_checkin_date = sdf.format(list1.get(i)
+                        .get("booking_checkin_date"));
+                String booking_checkout_date = sdf.format(list1.get(i)
+                        .get("booking_checkout_date"));
+                String created_at = sdf.format(list1.get(i).get("created_at"));
+                list1.get(i).put("booking_checkin_date", booking_checkin_date);
+                list1.get(i).put("booking_checkout_date", booking_checkout_date);
+                list1.get(i).put("created_at", created_at);
+            }
             List<Map<String, Object>> list2 = myBookingService.getMyBookingList(map2);
 
+            for (int i = 0; i < list2.size(); i++) {
+                String booking_checkin_date = sdf.format(list2.get(i)
+                        .get("booking_checkin_date"));
+                String booking_checkout_date = sdf.format(list2.get(i)
+                        .get("booking_checkout_date"));
+                String created_at = sdf.format(list2.get(i).get("created_at"));
+                list2.get(i).put("booking_checkin_date", booking_checkin_date);
+                list2.get(i).put("booking_checkout_date", booking_checkout_date);
+                list2.get(i).put("created_at", created_at);
+            }
+//            booking_checkin_date
+//            booking_checkout_date
+            // created_at
             // 뷰파일에 넘겨줄 model setting
             model.addAttribute("list1", list1);
             model.addAttribute("list2", list2);
