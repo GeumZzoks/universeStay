8f0659b67b36b1e3cb208f94c75a74bb2f32e3a9
jinkwon9301 <77374066+jinkwon9301@users.noreply.github.com>
2023-12-25 11:17:01 +0900

    feat: 숙소 등록페이지 에러처리 1 (#243)
    * fix: InquriyDto 경로 문제로 인한 톰캣 구동 문제 해결
    
    * fix: style.css 주석 처리 해제로 css 미적용 해결
    
    * hotfix: 버그 수정
    
    * fix : 오타 수정
    
    * hotfix: roomDetail.js 충돌 해결
    
    * feat: 예약하기 숫자 컴마 교정
    
    * fix: mybatisConfig 에러 수정
    
    * feat: 숙소 등록시 호스트 모드로 전환
    
    * fix: 숙소 상세페이지 스크립트 닫기 문제 해결
    
    * fix: 숙소 상세페이지 예약하기 추가인원 가격 에러 해결
    
    * feat: 숙소 등록페이지 JSP HTML & CSS 수정 : 필수입력 표기
    
    * feat: 숙소 등록 에러 처리 1
    
    * feat: 숙소 등록페이지 에러처리 1

--- src/main/java/com/universestay/project/room/dto/RoomDto.java
+++ src/main/java/com/universestay/project/room/dto/RoomDto.java
@@ -2,6 +2,7 @@ package com.universestay.project.room.dto;
 
 import io.swagger.annotations.ApiModelProperty;
 import java.sql.Timestamp;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,6 +21,7 @@ public class RoomDto {
     @ApiModelProperty(notes = "숙소번호 ID", example = "0ca24692-89ea-11ee-b9d1-0242ac120002", required = true)
     private String room_id;
 
+    @NotBlank
     @ApiModelProperty(notes = "숙소 카테고리 ID", example = "0ca24692-89ea-11ee-b9d1-0242ac120002", required = true)
     private String room_category_id;
 
@@ -32,9 +34,11 @@ public class RoomDto {
     @ApiModelProperty(notes = "숙소 상태", example = "R01", required = true)
     private String room_status_id;
 
+    @NotBlank
     @ApiModelProperty(notes = "숙소 이름", example = "남산레지던스", required = true)
     private String room_name;
 
+    @NotBlank
     @ApiModelProperty(notes = "숙소 주소", example = "0", required = true)
     private String room_address;
 
